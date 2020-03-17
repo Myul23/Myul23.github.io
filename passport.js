711d460e3ac5232eed448118295c4bf14a4b1d1d
Myul23 <62203859+Myul23@users.noreply.github.com>
2020-03-17 21:13:13 +0900

    Delete passport.js

--- passport.js
+++
@@ -1,8 +0,0 @@
-function LoginCheck() {
-    var point = document.getElementById("pass").value;
-    point = point.toLowerCase();
-    if ((point === "study") || (point === "lesson")) {
-        var change = "<a href='Review Lessons\\Entrance.html' target='_self'>Playground</a>";
-        document.getElementsByTagName("strong")[0].innerHTML = change;
-    } else if (point === "덕질");
-}

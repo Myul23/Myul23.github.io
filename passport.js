c6755a4558b0c447623d3111e913d919bdcb17f9
Myul23 <62203859+Myul23@users.noreply.github.com>
2020-03-21 17:12:55 +0900

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,15 +1,18 @@
+"use strict";
+LoginCheck();
+
 function LoginCheck() {
-    var point = document.getElementById("pass").value;
-    point = point.toLowerCase();
+	var point = document.getElementById("pass").value;
+	point = point.toLowerCase();
 
-    var change = "<a href='";
-    if (point === "sample") change += "0. Sample\\"; // 이거 생각보다 좋은데?
-    else if (point === "덕질") change += "A.G.O.S\\Entrance.html";
-    else if (point === "interesting") change += "Interesting\\";
-    else if ((point === "lesson") || (point === "lessons"))
-        change += "Review Lessons\\Core.html";
-    else change += "index.html";
-    change += "' target='_self'>Playground</a>";
+	var change = "<a href='";
+	if (point === "sample") change += "0. Sample\\"; // 이거 생각보다 좋은데?
+	else if (point === "덕질") change += "A.G.O.S\\Entrance.html";
+	else if (point === "interesting") change += "Interesting\\";
+	else if ((point === "lesson") || (point === "lessons"))
+		change += "Review Lessons\\Core.html";
+	else change += "index.html";
+	change += "' target='_self'>Playground</a>";
 
-    document.getElementsByTagName("strong")[0].innerHTML = change;
+	document.getElementsByTagName("strong")[0].innerHTML = change;
 }
